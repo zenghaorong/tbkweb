95e24dd92d9cc75897086fdff96ad54718fbd879
zenghaorong <2861574504@qq.com>
2020-03-07 22:01:14 +0800

    修改样式

--- tbk/themes/js/base.js
+++ tbk/themes/js/base.js
@@ -1,7 +1,7 @@
-var CTX = "http://localhost:8080";
+// var CTX = "http://localhost:8080";
 
 // var CTX = "http://192.168.0.101:8080";
 
 
 //线上地址
-// var CTX = "http://139.155.87.87:8080/aebiz";
+var CTX = "http://139.155.87.87:8080/aebiz";
