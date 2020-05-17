553ac777b9006ebdd9c703241b12db0d767da87a
zenghaorong <2861574504@qq.com>
2020-05-17 10:19:40 +0800

    提交一版 登录注册

--- tbk/themes/js/base.js
+++ tbk/themes/js/base.js
@@ -5,3 +5,14 @@
 
 //线上地址
 var CTX = "http://139.155.87.87:8080/aebiz";
+
+
+//获取传递的单个参数
+function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
